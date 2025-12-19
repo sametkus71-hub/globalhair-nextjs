c6b67be499b5a6cdc9b310806fe7b72be35d9be1
Samet <samet@globalhair.nl>
2025-12-19 05:27:10 +0100

    Fix build error: remove unsupported eslint config from next.config.js

--- next-app/next.config.js
+++ next-app/next.config.js
@@ -6,11 +6,6 @@ const nextConfig = {
     // your project has TypeScript type errors.
     ignoreBuildErrors: process.env.NODE_ENV === 'production',
   },
-  eslint: {
-    // Allow production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: process.env.NODE_ENV === 'production',
-  },
 };
 
 module.exports = nextConfig;
